464f8587a6a3ff69cefaa8e9ba5139f5e9a4dbba
Neron (Windows) <phearaneron.soeung@gmail.com>
2025-06-25 13:26:54 +0700

    footer

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ app = create_app()
 if __name__ == '__main__':
     print("➞ MAX_CONTENT_LENGTH:", app.config.get('MAX_CONTENT_LENGTH'))
     print("➞ Session storage enabled")
-app.run(debug=True, port=5001)
+    app.run(debug=True, port=5001)
